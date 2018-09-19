76f31469746e1843939650afb49ad33a82ea4ded
dee <dee@adadevelopersacademy.org>
2018-09-19 11:21:23 -0700

    replace index/show routes with manual entry of restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # the "as: 'books'" part is defining a NICKNAME for a path (in this case: '/books')
+
+  # Therefore, I can refer to this path '/books' with the NICKNAME formed by the pattern: books_path (which is the nickname + _path)
+  get '/books', to: 'books#index', as: 'books'
+
+  # The nickname of this path '/books/new' is: new_book_path
+  get '/books/new', to: 'books#new', as: 'new_book'
+  post '/books', to: 'books#create'
+
+  # book_path
+  get '/books/:id', to: 'books#show', as: 'book'
+
+  # edit_book_path
+  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+
+  patch '/books/:id', to: 'books#update'
+  delete '/books/:id', to: 'books#destroy'
 
 
-  # verb '/path', to: 'controller#action'
-  get '/books', to: 'books#index'
+  # Very possible:
+  # patch '/user/:user_id/books/:id', to: 'books#mark_read', as: 'mark_read'
 
-  # This route takes a parameter!
-  # It will accept _anything_ as :id
-  get '/books/:id', to: 'books#show'
 end
