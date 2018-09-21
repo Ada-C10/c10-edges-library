97f0ed744da053597c494f0c963d2cf4d78b5c15
Dan Roberts <dan@adadevelopersacademy.org>
2018-09-21 11:40:09 -0700

    Use resources instead of manually specifying routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,31 @@ Rails.application.routes.draw do
   # Root route says what do I get for '/'
   root 'books#index'
 
+  # Auto-generate all the RESTful routes
+  resources :books, except: [:edit, :update]
+
+  # Non-RESTful routes are OK, so long as you have
+  # non-RESTful work to do
+  post '/books/:id/check_out', to: 'books#check_out', as: 'check_out_book'
+
   # the "as: 'books'" part is defining a NICKNAME for a path (in this case: '/books')
 
   # Therefore, I can refer to this path '/books' with the NICKNAME formed by the pattern: books_path (which is the nickname + _path)
-  get '/books', to: 'books#index', as: 'books'
+  # get '/books', to: 'books#index', as: 'books'
+  #
+  # # The nickname of this path '/books/new' is: new_book_path
+  # get '/books/new', to: 'books#new', as: 'new_book'
+  # post '/books', to: 'books#create'
+  #
+  # # book_path
+  # get '/books/:id', to: 'books#show', as: 'book'
+  #
+  # # edit_book_path
+  # get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+  # patch '/books/:id', to: 'books#update'
+  #
+  # delete '/books/:id', to: 'books#destroy'
 
-  # The nickname of this path '/books/new' is: new_book_path
-  get '/books/new', to: 'books#new', as: 'new_book'
-  post '/books', to: 'books#create'
-
-  # book_path
-  get '/books/:id', to: 'books#show', as: 'book'
-
-  # edit_book_path
-  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
-  patch '/books/:id', to: 'books#update'
-
-  delete '/books/:id', to: 'books#destroy'
 
 
   # Very possible:
