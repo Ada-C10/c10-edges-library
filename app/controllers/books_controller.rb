ce0231c5efcc6fa6e22fe18ecc9293906c110407
Dan Roberts <dan@adadevelopersacademy.org>
2018-09-21 11:40:25 -0700

    Strong params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,10 +15,8 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(
-      author: params[:book][:author],
-      title: params[:book][:title]
-    )
+    filtered_book_params = book_params()
+    @book = Book.new(filtered_book_params)
 
     is_successful_save = @book.save
 
@@ -29,10 +27,28 @@ class BooksController < ApplicationController
     end
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+  end
+
   def destroy
     book = Book.find_by(id: params[:id])
 
     book.destroy
     redirect_to books_path
+  end
+
+  private
+
+  # Strong params: only let certain attributes
+  # through
+  def book_params
+    return params.require(:book).permit(
+      :title,
+      :author,
+      :description,
+      :isbn
+    )
   end
 end
