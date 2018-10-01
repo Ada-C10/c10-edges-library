ab8281a7e67d3710c0bdbad22e6e6c3191d27d48
Dan Roberts <dan@adadevelopersacademy.org>
2018-10-01 14:54:49 -0700

    Author.first_published

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,9 @@
 class Author < ApplicationRecord
   has_many :books
+
+  def first_published
+    books_with_year = self.books.where.not(pub_date: nil)
+    first_book = books_with_year.order(pub_date: :asc).first
+    return first_book ? first_book.pub_date : nil
+  end
 end
