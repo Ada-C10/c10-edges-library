5ca542a8ca9e23587e97082d8cf9cbd971613710
dee <dee@adadevelopersacademy.org>
2018-10-05 11:10:57 -0700

    changes db so there is a join table between books and genres

---
+++ db/migrate/20181005175856_create_books_genres_join.rb
@@ -0,0 +1,10 @@
+class CreateBooksGenresJoin < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books_genres do |t|
+
+      t.belongs_to :book, index: true
+      t.belongs_to :genre, index: true
+
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_174457) do
+ActiveRecord::Schema.define(version: 2018_10_05_175856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2018_10_05_174457) do
     t.string "isbn"
     t.bigint "author_id"
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "books_genres", force: :cascade do |t|
+    t.bigint "book_id"
+    t.bigint "genre_id"
+    t.index ["book_id"], name: "index_books_genres_on_book_id"
+    t.index ["genre_id"], name: "index_books_genres_on_genre_id"
   end
 
   create_table "genres", force: :cascade do |t|
