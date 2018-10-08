7afcca046200a44233911649be5f477b8329ecf6
dee <dee@adadevelopersacademy.org>
2018-10-08 11:05:10 -0700

    tests that books are invalid without a title

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -7,27 +7,32 @@ describe Book do
 
   describe 'validations' do
 
-    # For every model, have one test that says everything is good! and valid! sometimes!
-    # Make a new book with a valid title (present and unique)
-    # calling .valid? on it will be true
-
-    it 'is valid when title is present and unique and author is present' do
-      # Arrange
+    before do
       author = Author.new(name: 'test author')
-      book = Book.new(title: 'test book', author: author)
+      @book = Book.new(title: 'test book', author: author)
+    end
 
-      # Act
-      is_valid = book.valid?
+    # For every model, have one test that says everything is good! and valid! sometimes!
+    it 'is valid when title is present and unique and author is present' do
 
-      # Assert
+      is_valid = @book.valid?
       expect( is_valid ).must_equal true
+
+    end
+
+    # A book that doesn't have a title is not valid
+    it 'is invalid without a title' do
+
+      @book.title = nil
+
+      is_valid = @book.valid?
+
+      expect( is_valid ).must_equal false
+      expect( @book.errors.messages ).must_include :title
+
     end
 
   end
-
-  # A book that doesn't have a title is not valid
-  # A book with a title of nil value or not provided will...
-  # when calling .valid? will return false
 
   # A book that has a not-unique title is not valid
   # There is an instance of Book (first book)
