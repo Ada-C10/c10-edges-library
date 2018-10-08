da2221c4417e0f3c5579851890fb4bfcb13f3955
dee <dee@adadevelopersacademy.org>
2018-10-08 11:25:34 -0700

    starts the test for uniqueness on book titles

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -12,7 +12,6 @@ describe Book do
       @book = Book.new(title: 'test book', author: author)
     end
 
-    # For every model, have one test that says everything is good! and valid! sometimes!
     it 'is valid when title is present and unique and author is present' do
 
       is_valid = @book.valid?
@@ -20,24 +19,29 @@ describe Book do
 
     end
 
-    # A book that doesn't have a title is not valid
     it 'is invalid without a title' do
-
       @book.title = nil
 
       is_valid = @book.valid?
 
       expect( is_valid ).must_equal false
       expect( @book.errors.messages ).must_include :title
+    end
 
+    # A book that has a not-unique title is not valid
+    # There is an instance of Book (first book)
+    # There is an instance of Book (second book), both have same titles
+    # Calling .valid? on second book will return false
+    it 'is invalid with a non-unique title' do
+      second_book = Book.new(title: @book.title, author: Author.new(name: 'foo'))
+
+      is_valid = second_book.valid?
+
+      # expect( is_valid ).must_equal false
     end
 
   end
 
-  # A book that has a not-unique title is not valid
-  # There is an instance of Book (first book)
-  # There is an instance of Book (second book), both have same titles
-  # Calling .valid? on second book will return false
 
 
 
