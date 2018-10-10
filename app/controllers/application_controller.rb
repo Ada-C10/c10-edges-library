e7ff2bcfae5840d5aeb9c05e892b1b8562270eb9
dee <dee@adadevelopersacademy.org>
2018-10-10 15:32:20 -0700

    finding a logged in user is now a controller filter in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  before_action :find_logged_in_author
+
+  private
+
+  def find_logged_in_author
+    @logged_in_author = Author.find_by(id: session[:user_id])
+  end
+
 end
