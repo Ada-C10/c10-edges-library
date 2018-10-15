96cdbf5da923b1eeac3d1ce0234babb5513985c1
dee <dee@adadevelopersacademy.org>
2018-10-15 11:06:30 -0700

    show action in books controller tests

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -10,4 +10,36 @@ describe BooksController do
     must_respond_with :success
   end
 
+  describe "show" do
+
+    it "should respond with success for showing an existing book" do
+      # Arrange
+      existing_book = books(:poodr)
+
+      # Act
+      get book_path(existing_book.id)
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "should respond with not found for showing a non-existing book" do
+      book = books(:poodr)
+      id = book.id
+
+      get book_path(id)
+      must_respond_with :success
+
+
+      book.destroy
+
+      get book_path(id)
+
+      must_respond_with :missing
+    end
+
+  end
+
+
+
 end
