6eca8ceebe8b3034e032252c5e5042ab83ee6a76
Dan Roberts <dan@adadevelopersacademy.org>
2018-10-16 14:45:56 -0700

    Login/logout UI element

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
 
-  before_action :find_logged_in_author
+  before_action :find_logged_in_user
 
   private
 
-  def find_logged_in_author
-    @logged_in_author = Author.find_by(id: session[:user_id])
+  def find_logged_in_user
+    @logged_in_user = User.find_by(id: session[:user_id])
   end
 
 end
